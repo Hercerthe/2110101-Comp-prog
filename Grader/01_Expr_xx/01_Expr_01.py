286a6617ad5ea32a3e9b50e91c5130dada36a741
TanatV <94076487+jjupiterr@users.noreply.github.com>
2022-08-24 20:46:40 +0700

    Delete Grader/01_Expr_xx directory

--- Grader/01_Expr_xx/01_Expr_01.py
+++
@@ -1,8 +0,0 @@
-import math
-
-n = int(input())
-lowerline = (math.sqrt(2*math.pi))*(n**(n+0.5))*(math.e**(-n+(1/(12*n+1))))
-upperline = (math.sqrt(2*math.pi))*(n**(n+0.5))*(math.e**(-n+(1/(12*n))))
-
-print("Lowerline is",lowerline)
-print("Upperline is",upperline)
